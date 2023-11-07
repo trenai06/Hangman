e50f766a8b1403a12cea01fa6dfc0f4c0d85cb2f
Cheniqua Breaux <cbreaux2@mkc02dq300md6r.humad.com>
2023-11-07 17:15:45 -0500

    first commit and most of styling done

---
+++ script.js
@@ -0,0 +1,12 @@
+wordBank = ["JUVENILE", "COURTESY", "LISTEN", "CATCALL", "LEPROSY"]
+
+// 1. Create wordbank (array) randomize, push on load
+// 2. create an empty array for letter blanks of the called word (push on load) 
+// 3. when a letter is chosen and correct it appears in word space
+// 4. when letter is incorrect blank out with same background color 
+// 5. when letter is incorrect draw a piece of the man 
+// 6. game win alert and reload
+// 7. game lose alert and reload
+// 8. create canvas for hangman draw
+// 9. fine tune Css Styling  .remove border from the word spell out space 
+// 10. maybe a for each function for letter buttons
